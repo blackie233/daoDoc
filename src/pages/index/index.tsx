29a86e648131ec3f7d34b05c80956439f9b3862a
xuejiangtao <1244882992@qq.com>
2024-04-17 23:02:54 +0800

    demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,42 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import {View} from '@tarojs/components'
+import Taro from "@tarojs/taro";
+import {AtSearchBar, AtTabBar} from "taro-ui";
 import './index.scss'
+import {Component} from "react";
 
-export default function Index() {
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+class Index extends Component<any, any> {
 
-  return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
-  )
+    switchTab(value) {
+        if (value !== 1) return;
+        Taro.reLaunch({
+            url: "/pages/my/index"
+        });
+    }
+
+    render() {
+        return (
+            <View className='container'>
+                <AtSearchBar
+                    actionName= '搜索'
+                    value={""}
+                    onChange={() => {
+                    }}>
+                </AtSearchBar>
+                <AtTabBar fixed
+                          selectedColor="#d43c33"
+                          tabList={[
+                              {title: "求卦", iconPrefixClass: "fa", iconType: "feed"},
+                              {title: "查卦", iconPrefixClass: "fa", iconType: "music"},
+                              {title: "个人", iconPrefixClass: "fa", iconType: "music"}
+                          ]}
+                          current={0} onClick={this.switchTab.bind(this)}>
+                </AtTabBar>
+            </View>
+        )
+    }
 }
+
+export default Index;
+
+
